9035805a88b8a829c9819b28dd7ff4bd95aea819
ank-21 <ankitSrivastava21598@gmail.com>
2019-10-27 23:37:25 +0530

    path module added

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
+const path = require('path')
 
 const app = express() 
 app.use(express.json())
 
-const publicDirectoryPath = path.join(__dirname,'../public')
+const publicDirectoryPath = path.join(__dirname,'/public')
 app.use(express.static(publicDirectoryPath))
 
 

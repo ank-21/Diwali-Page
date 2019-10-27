e49b5198b59be89cb4f054fb16f982ba3c79dfa1
ank-21 <ankitSrivastava21598@gmail.com>
2019-10-27 21:39:07 +0530

    updated port

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+
+const app = express() 
+app.use(express.json())
+
+const publicDirectoryPath = path.join(__dirname,'../public')
+app.use(express.static(publicDirectoryPath))
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log('Server is up on port : '+ port)
+})
